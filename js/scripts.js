b9753e1afa3485c13ff623cb58e79a8fad829891
Student <example@example.com>
2019-11-06 10:12:15 -0800

    Dice rolling functionality works, creates two objects, one for each player

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+//Business Logic
+// 1. Random number generator to create a number between 1 and 6: Variable to hold that current result.
+// 2. Variable to contain the result of the first roll and all addtitional rolls provided the user doesn't roll a 1. if they roll a 1 this number goes to zero.
+//
+// 3. Hold button to allow the current user to switch players and bank their "total score" for that round.
+// 4. Roll button to allow the user to roll another number. (connected to 1 and 2).
+// 5. Alert or HTML change when a player hits 100 first.
+//Toggle correct player between Zero and One to indicate current player
+
+var randomNumber;
+var currentResult = 0;
+var currentPlayerTotal;
+
+//need variable to old whichever object total we are currently focused on
+
+
+
+
+function updateCurrentResult(){
+  randomNumber = Math.floor((Math.random() * 6) + 1);
+  console.log(randomNumber);
+  if(randomNumber === 1){
+    currentResult = 0;
+    //statement here switching from Player1 to Player2;
+
+    //add code here to switch to other player
+  }else{
+    currentResult += randomNumber;
+  }
+};
+
+function hold(){
+
+  //total area of respective object + currentResult;
+  currentPlayerTotal;//Player1 or Player2
+
+
+}
+
+Player = function(name){
+
+this.name = name,
+this.playerTotal = 0
+
+}
+GameManager = function(){
+
+this.players = [];
+
+}
+
+GameManager.prototype.addPlayer = function (player){
+
+//need variable called "newPlayer"
+this.players.push(player);
+
+}
+//this works
+// var newPlayer = new Player("juan");
+var gameManager = new GameManager();
+//
+//
+// gameManager.addPlayer(newPlayer);
+// console.log(gameManager.players);
+
+//this works ^
+
+// gameManager.addPlayer()
+
+
+
+
+
+// addPlayer(newPlayer);
+//
+// console.log(GameManager.players);
+
+
+
+
+// var player1 = new Player("Juan");
+
+// console.log(player1);
+
+
+//UI
+
+// 1. Form field when a user starts the game. The form submission will create 2 new objects and push them into an array. We will use this array to switch between the first and second players.
+// 2. Hide the form and show the game when the player submits.
+
+$(document).ready(function(){
+  $(".form-input").submit(function(event){
+    event.preventDefault();
+    var newPlayer1 = $("#addPlayer1").val();
+    var newPlayer2 = $("#addPlayer2").val();
+
+    var newPlayer1 = new Player(newPlayer1);
+    gameManager.addPlayer(newPlayer1);
+    var newPlayer2 = new Player(newPlayer2);
+    gameManager.addPlayer(newPlayer2);
+    console.log(gameManager.players);
+
+
+
+
+  });
+});
